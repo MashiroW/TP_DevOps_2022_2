909b3c82dbbd77fcb93ebe9ad63a72b464aed8be
MashiroW <youssefort@gmail.com>
2022-03-16 10:57:52 +0100

    first commit

--- choise.py
+++ choise.py
@@ -45,17 +45,16 @@ words = [
     "42",
 ]
 
-
-firstquestion = random.randint(0,5)
+firstquestion = random.randint(0, 5)
 secondquestion = firstquestion
 
 while secondquestion == firstquestion:
-  secondquestion = random.randint(0,5)
+	secondquestion = random.randint(0, 5)
 
 thirdquestion = firstquestion
 
 while thirdquestion == firstquestion or thirdquestion == secondquestion:
-  thirdquestion = random.randint(0,5)
+	thirdquestion = random.randint(0, 5)
 
 print("Firstquestion :")
 print(words[firstquestion])
@@ -64,13 +63,16 @@ print(words[secondquestion])
 print("Thirdquestion :")
 print(words[thirdquestion])
 
-#input file
+# input file
 fin = open("data.txt", "rt")
-#output file to write the result to
+# output file to write the result to
 fout = open("index.php", "wt")
-#for each line in the input file
+# for each line in the input file
 for line in fin:
-    fout.write(line.replace('question1', words[firstquestion]).replace('question2', words[secondquestion]).replace('question3', words[thirdquestion]))
-#close input and output files
+	my_str = line.replace('question1', words[firstquestion]).replace('question2', words[secondquestion])
+	my_str = my_str.replace('question3', words[thirdquestion])
+	fout.write(my_str)
+# close input and output files
 fin.close()
 fout.close()
+
